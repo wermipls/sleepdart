2896f5436d667c6bf63836eb016adcd9aac31b37
wermi <32251376+wermipls@users.noreply.github.com>
2023-04-21 20:25:18 +0200

    update ayumi to use faster processing func

--- ay.c
+++ ay.c
@@ -87,7 +87,7 @@ void ay_write_data(AY_t *ay, uint8_t value)
 
     uint64_t write = ay->ctx->cpu.cycles / ay->samples_ratio;
     for ( ; ay->last_write < write; ay->last_write++) {
-        ayumi_process(&ay->ayumi);
+        ayumi_process_fast(&ay->ayumi);
         ayumi_remove_dc(&ay->ayumi);
         ay->buf[ay->buf_pos] = 0.5 * ay->ayumi.left;
         ay->buf[ay->buf_pos+1] = 0.5 * ay->ayumi.right;
@@ -103,7 +103,7 @@ uint8_t ay_read_data(AY_t *ay)
 void ay_process_frame(AY_t *ay)
 {
     for ( ; ay->last_write < ay->samples_frame; ay->last_write++) {
-        ayumi_process(&ay->ayumi);
+        ayumi_process_fast(&ay->ayumi);
         ayumi_remove_dc(&ay->ayumi);
         ay->buf[ay->buf_pos] = 0.5 * ay->ayumi.left;
         ay->buf[ay->buf_pos+1] = 0.5 * ay->ayumi.right;
