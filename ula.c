9647fddff8439e85793d141cae19242e737b5577
wermi <32251376+wermipls@users.noreply.github.com>
2023-04-25 10:55:13 +0200

    fix off-by-1 in contention pattern

--- ula.c
+++ ula.c
@@ -108,7 +108,7 @@ uint8_t ula_get_contention_cycles(uint64_t cycle)
     if (line > 192) return 0;
 
     uint16_t linecyc = cycle % timing.t_scanline;
-    if (linecyc > timing.t_screen) return 0;
+    if (linecyc >= timing.t_screen) return 0;
 
     return contention_pattern[linecyc % 8];
 }
